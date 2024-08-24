4a71ab023a35373b38c52050d36a189a8defbdb9
Marcos Silva <marcossilva@Marcoss-Notebook.local>
2024-08-24 09:27:17 -0400

    create swagger documentation

--- src/main/java/br/com/marcosilva/gestao_de_vagas/ManagementJobPositionApplication.java
+++ src/main/java/br/com/marcosilva/gestao_de_vagas/ManagementJobPositionApplication.java
@@ -3,7 +3,13 @@ package br.com.marcosilva.gestao_de_vagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(title = "position management", description = "API - responsible for position management", version = "1")
+)
 public class ManagementJobPositionApplication {
 
 	public static void main(String[] args) {
